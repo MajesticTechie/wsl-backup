c76ad2c354bc9c22ef8bf80ae025c7e97c8deb44
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2021-05-31 15:40:44 +0100

    Update export.sh

--- export.sh
+++ export.sh
@@ -3,6 +3,9 @@
 # Create "wsl-backup" folder on the C drive
 mkdir /mnt/c/wsl-backup
 
+#clear backup folder (if used before)
+rm -rf /mnt/c/wsl-backup/*
+
 #Backup SSH keys
 cp -pr .ssh/ /mnt/c/wsl-backup/
 
