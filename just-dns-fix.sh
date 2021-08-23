b15fc55b39c325c98bd5cce190c9e99a6b9fb683
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2021-08-23 13:04:07 +0100

    Update just-dns-fix.sh
    #!bin/bash
    
    # Set Manual DNS
    sudo tee /etc/wsl.conf >/dev/null <<EOF
    [network]
    generateResolvConf = false
    EOF
    
    
    #Delete existing file
    sudo rm /etc/resolv.conf
    
    # regenerate resolv.conf with preferred resolvers
    sudo tee /etc/resolv.conf >/dev/null <<EOF
    nameserver 1.1.1.1
    nameserver 9.9.9.9
    EOF

--- just-dns-fix.sh
+++ just-dns-fix.sh
@@ -1,1 +1,17 @@
+#!bin/bash
 
+# Set Manual DNS
+sudo tee /etc/wsl.conf >/dev/null <<EOF
+[network]
+generateResolvConf = false
+EOF
+
+
+#Delete existing file
+sudo rm /etc/resolv.conf
+
+# regenerate resolv.conf with preferred resolvers
+sudo tee /etc/resolv.conf >/dev/null <<EOF
+nameserver 1.1.1.1
+nameserver 9.9.9.9
+EOF
